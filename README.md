5e750c03c146a73570aeee36e26d0f674fa47499
DreamPiggy <lizhuoli1126@126.com>
2023-11-01 18:11:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+### Radar FB13322401
+
+Related issue report:
+
+https://github.com/SDWebImage/SDWebImage/issues/3605
+
+### Title
+
+iOS 17 Indexed PNG decode wrong which report alpha info to non-premultiplied RGBA, but bitmap array is premultiplied
+
+### Description
+
+From iOS 17/macOS 14, there is one serious problem on ImageIO PNG plugin. The decode result for indexed color PNG use the wrong CGImageAlphaInfo
+
+The returned CGImageAlphaInfo is alpha last, but the actual bitmap data is premultiplied alpha first, which cause many runtime render bug.
+
+I already submit a radar FB13196663 to UIKit team, but seems iOS 17.2 still contains this issue. After digging into details, I think this radar should reported to ImageIO team.
+
+### Steps to reproduce
+
+1. Just download my “iOS17IndexedPNGDecodeBug.zip” in attachment, which provide a one pixel PNG image
+2. Open “iOS17IndexedPNGDecodeBug.xcodeproj”
+3. Run with destination on iOS 16 device or simulator, no assertion failed. The pixel value (50, 50, 50, 50)
+4. Run with destination on iOS 16 device or simulator, hit assertion, because the pixel value (10, 10, 10, 50), which looks like the premultiplied result 
+
+### Excepted behavior
+Both iOS 17 and iOS 16 decode result for that indexed color PNG should match. Well ImageIO team can use premultiplied RGBA, but please, you at least should create `CGImage` with the correct CGImageAlphaInfo.
+
+Currently we have no good workaround to solve this, this break many render pipeline when convert the ImageIO decoded CGImage into other pixel buffers.
+
+### Screenshot
+
+#### iOS 16
+
+![iOS 16 Screenshot](assets/iOS%2016%20Screenshot.png)
+
+
+#### iOS 17
+
+![iOS 17 Screenshot](assets/iOS%2017%20Screenshot.png)
+
--- iOS17IndexedPNGDecodeBug/ViewController.swift
+++ iOS17IndexedPNGDecodeBug/ViewController.swift
@@ -8,10 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func loadPNGBitmap(_ data: Data) {
+        let options: CFDictionary? = nil
+        let source = CGImageSourceCreateWithData(data as CFData, options)!
+        let frameOptions: CFDictionary? = nil
+        let cgImage = CGImageSourceCreateImageAtIndex(source, 0, frameOptions)!
+        let alphaInfo = cgImage.alphaInfo
+        
+        // The returned CGImage Alpha Info is `.last` on both iOS 16/iOS 17
+        // But on iOS 17, actually the bitmap data vector is always **premultiplied**
+        assert(alphaInfo == .last)
+        
+        // Let's check whether it's premultiplied or not
+        let bitmapData = cgImage.dataProvider?.data!
+        let length = CFDataGetLength(bitmapData)
+        var rawData = [UInt8].init(repeating: 0, count: length)
+        CFDataGetBytes(bitmapData, CFRange(location: 0, length: length), &rawData)
+        // Check first 1 pixels one by one
+        assert(length == 1 * 4)
+        
+        // The I tested is RGBA(50, 50, 50, 50), non-premultiplied
+        let R = rawData[0]
+        let G = rawData[1]
+        let B = rawData[2]
+        let A = rawData[3]
+        // assert failed on >= iOS 17, success on iOS 16
+        assert(R == 50)
+        assert(G == 50)
+        assert(B == 50)
+        assert(A == 50)
+        
+        print("Success load one pixel indexed PNG")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = Bundle.main.url(forResource: "IndexedOnePixelImage", withExtension: "png")!
+        let pngData = try! Data(contentsOf: url)
+        loadPNGBitmap(pngData)
     }
 
 
